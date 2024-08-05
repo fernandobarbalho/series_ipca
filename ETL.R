a161c3a6f3cd66185dc0844ed5622e3fe809467a
Fernando Almeida Barbalho <40247971+fernandobarbalho@users.noreply.github.com>
2024-08-05 16:44:27 -0300

    Aprimoramento do cálculo de números índices

--- ETL.R
+++ ETL.R
@@ -24,14 +24,35 @@ writexl::write_xlsx(variacao_trabalho, "series_ipca.xlsx")
 
 variacao_trabalho$variacao_acumulada <- 100
 
-fab<-
-  variacao_trabalho %>%
-  group_by(code) %>%
-  mutate(lag_value = lag(value, n=1)) %>%
-  mutate(variacao_acumulada =  ifelse(is.na(lag_value), 100 * (1+value/100), lag(variacao_acumulada, n=1) * (1+value/100)))
-
 
 
 series_ipca_numero_indice <- read_excel("series_ipca_numero_indice.xlsx")
+
+saveRDS(series_ipca_numero_indice, "series_ipca_numero_indice.rds")
+
+
+series_ipca_numero_indice<-
+purrr::map_dfr(unique(variacao_trabalho$code),function(codigo){
+  print(codigo)
+  
+  IPCA_historico<- 
+    variacao_trabalho %>%
+    filter(code == codigo)
+  
+  num_indice_aux<-
+  map_dbl(1:NROW(IPCA_historico), function(a_i){
+    
+    print(a_i)
+    prod(1+IPCA_historico$value[1:a_i]/100)
+    
+  })
+  
+  
+  #IPCA_historico$num_indice_aux <- num_indice_aux
+  
+  IPCA_historico$numero_indice =  100 * num_indice_aux#IPCA_historico$num_indice_aux
+    
+  IPCA_historico
+})
 
 saveRDS(series_ipca_numero_indice, "series_ipca_numero_indice.rds")
